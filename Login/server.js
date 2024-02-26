e2efff0a29ecccb68e49f8d5a46de11a4eb52066
Sri-Krishna-R-Hebbar <140789247+Sri-Krishna-R-Hebbar@users.noreply.github.com>
2024-02-26 20:15:44 +0530

    Update server.js

--- Login/server.js
+++ Login/server.js
@@ -12,15 +12,16 @@ app.use(json());
 
 app.use(express.static('public'));
 
+// Enter your details from Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBLIh3X4M7oMQaju2Li9exSvp-2iikyQfI",
-  authDomain: "login-and-signup-for-lawminds.firebaseapp.com",
-  databaseURL: "https://login-and-signup-for-lawminds-default-rtdb.firebaseio.com",
-  projectId: "login-and-signup-for-lawminds",
-  storageBucket: "login-and-signup-for-lawminds.appspot.com",
-  messagingSenderId: "153586382332",
-  appId: "1:153586382332:web:10106dbafc7cac07e26e79",
-  measurementId: "G-YCDVESSFCV"
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 initializeApp(firebaseConfig);
 
@@ -98,110 +99,3 @@ app.post('/client/login', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-
-// import express from "express";
-// import bodyParser from "body-parser";
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBLIh3X4M7oMQaju2Li9exSvp-2iikyQfI",
-//   authDomain: "login-and-signup-for-lawminds.firebaseapp.com",
-//   databaseURL: "https://login-and-signup-for-lawminds-default-rtdb.firebaseio.com",
-//   projectId: "login-and-signup-for-lawminds",
-//   storageBucket: "login-and-signup-for-lawminds.appspot.com",
-//   messagingSenderId: "153586382332",
-//   appId: "1:153586382332:web:10106dbafc7cac07e26e79",
-//   measurementId: "G-YCDVESSFCV"
-// };
-
-// const app = express();
-// const port = 3000;
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// app.use(express.static('public'));
-
-// // Initialize Firebase
-// const firebaseApp = initializeApp(firebaseConfig);
-// const auth = getAuth(firebaseApp);
-// const database = getDatabase(firebaseApp);
-
-// // Routes
-// app.post('/lawyer/signup', async (req, res) => {
-//   const { firstName, lastName, email, barristerId, password } = req.body;
-
-//   try {
-//     const userCredential = await auth.createUserWithEmailAndPassword(email, password);
-//     const user = userCredential.user;
-
-//     // Save user data to the database
-//     await database.ref('lawyers/' + user.uid).set({
-//       firstName: firstName,
-//       lastName: lastName,
-//       email: email,
-//       barristerId: barristerId
-//     });
-
-//     res.status(200).send('Lawyer signed up successfully');
-//   } catch (error) {
-//     console.error('Lawyer signup error:', error.message);
-//     res.status(500).send('Error signing up lawyer');
-//   }
-// });
-
-// app.post('/lawyer/login', async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const userCredential = await auth.signInWithEmailAndPassword(email, password);
-//     const user = userCredential.user;
-
-//     res.status(200).send('Lawyer logged in successfully');
-//   } catch (error) {
-//     console.error('Lawyer login error:', error.message);
-//     res.status(500).send('Error logging in lawyer');
-//   }
-// });
-
-// app.post('/client/signup', async (req, res) => {
-//   const { firstName, lastName, email, password } = req.body;
-
-//   try {
-//     const userCredential = await auth.createUserWithEmailAndPassword(email, password);
-//     const user = userCredential.user;
-
-//     // Save user data to the database
-//     await database.ref('clients/' + user.uid).set({
-//       firstName: firstName,
-//       lastName: lastName,
-//       email: email
-//     });
-
-//     res.status(200).send('Client signed up successfully');
-//   } catch (error) {
-//     console.error('Client signup error:', error.message);
-//     res.status(500).send('Error signing up client');
-//   }
-// });
-
-// app.post("/client/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const userCredential = await auth.signInWithEmailAndPassword(email, password);
-//     const user = userCredential.user;
-
-//     res.status(200).send("Client logged in successfully.");
-//   } catch (error) {
-//     console.error("Client login error:", error.message);
-//     res.status(500).send("Error logging in client.");
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is listening at http://localhost:${port}`);
-// });
